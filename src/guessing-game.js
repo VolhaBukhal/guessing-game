585285236389dd0e23050286485bdd6f09f3a00d
ola <o.bukhal@gmail.com>
2021-08-17 15:51:32 +0300

    feat: add logic to guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,38 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.arr = [],
+        this.low = 0,
+        this.high,
+        this.middle
+    }
     setRange(min, max) {
-
+        for (let i = min; i <= max; i++){
+            this.arr.push(i);
+        }
+        // this.low = this.arr[0];
+        this.high = this.arr.length -1;
+        return this.arr;
     }
 
     guess() {
 
+        this.middle = Math.ceil( (this.high + this.low) / 2);
+        return this.middle;
     }
 
     lower() {
-
+        this.high  = this.middle ;
+        return this.high
     }
 
     greater() {
-
+        this.low = this.middle ;
+        return this.low;
     }
 }
+
+
+
+
 
 module.exports = GuessingGame;
